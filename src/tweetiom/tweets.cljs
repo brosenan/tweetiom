444663df02e6a2b0d55b6c61dff1d32644341720
Boaz Rosenan <brosenan@gmail.com>
2017-10-18 09:02:07 +0300

    atom internal to tweet-editor

--- src/tweetiom/tweets.cljs
+++ src/tweetiom/tweets.cljs
@@ -3,15 +3,15 @@
             [axiom-cljs.core :as ax])
   (:require-macros [axiom-cljs.macros :refer [defview defquery user]]))
 
-(defonce tweet-text (r/atom ""))
-
 (defview tweet-view [user]
   [:tweetiom/tweet user tweet ts])
 
 (defn tweet-editor [host]
-  (let [{:keys [add]} (meta (tweet-view host (user host)))]
-    [:div
-     [:input {:value @tweet-text
-              :on-change #(reset! tweet-text (.-target.value %))}]
-     [:button {:on-click #(add {:tweet [:tweet @tweet-text]
-                                :ts ((:time host))})} "Tweet"]]))
+  (let [{:keys [add]} (meta (tweet-view host (user host)))
+        tweet-text (atom "")]
+    (fn [host]
+      [:div
+       [:input {:value @tweet-text
+                :on-change #(reset! tweet-text (.-target.value %))}]
+       [:button {:on-click #(add {:tweet [:tweet @tweet-text]
+                                  :ts ((:time host))})} "Tweet"]])))
--- test/tweetiom/tweets_test.cljs
+++ test/tweetiom/tweets_test.cljs
@@ -4,32 +4,30 @@
             [reagent-query.core :as rq]
             [axiom-cljs.core :as ax]))
 
-;; The tweet editor contains an :input box and a "Tweet" :button.
+;; The tweet-editor function returns a ui function that renders an :input box and a "Tweet" :button.
 (deftest tweet-editor-1
   (let [host (ax/mock-connection "alice")
         ui (tweets/tweet-editor host)]
-    (is (= (rq/find ui :input:value) [""]))
-    (is (= (rq/find ui :button) ["Tweet"]))))
+    (is (= (rq/find (ui host) :input:value) [""]))
+    (is (= (rq/find (ui host) :button) ["Tweet"]))))
 
-;; The value of the :input box is bound with the tweet-text atom
+;; The :input box's :on-change callback updates its :value
 (deftest tweet-editor-2
-  (let [host (ax/mock-connection "alice")]
-    (reset! tweets/tweet-text "some-text")
-    (is (= (-> (tweets/tweet-editor host)
-               (rq/find :input:value)) ["some-text"]))
+  (let [host (ax/mock-connection "alice")
+        ui (tweets/tweet-editor host)]
     ;; If we invoke the :on-change callback we cause the tweet-text atom to change
-    (let [[on-change] (-> (tweets/tweet-editor host)
-                          (rq/find :input:on-change))]
-      (on-change (rq/mock-change-event "some-other-text"))
-      (is (= @tweets/tweet-text "some-other-text")))))
+    (let [[on-change] (rq/find (ui host) :input:on-change)]
+      (on-change (rq/mock-change-event "some-text"))
+      (is (= (rq/find (ui host) :input:value) ["some-text"])))))
 
 ;; Clicking the Tweet :button creates a new tweet in the tweet-view
 (deftest tweets-view-3
   (let [host (-> (ax/mock-connection "alice")
                  (assoc :time (constantly 12345)))]
-    (let [[on-click] (-> (tweets/tweet-editor host)
-                         (rq/find :button:on-click))]
-      (reset! tweets/tweet-text "the-tweet-to-submit")
+    (let [ui (tweets/tweet-editor host)
+          [on-change] (rq/find (ui host) :input:on-change)
+          [on-click] (rq/find (ui host) :button:on-click)]
+      (on-change (rq/mock-change-event "the-tweet-to-submit"))
       (on-click)
       (let [tweets (tweets/tweet-view host "alice")]
         ;; The tweet itself is the term [:tweet T], where T is the tweet's text
